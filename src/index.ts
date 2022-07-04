2f7c23cc41af784b8c3448b686e99226be4199f8
madness <ss@gmail.com>
2022-07-04 17:06:49 +0300

    index.ts support

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import useBool from './useBool'
-import useOnce from './useOnce'
-import useSetFalse from './useSetFalse'
-import useSetTrue from './useSetTrue'
-import useToggle from './useToggle'
-
-export default {useBool,useOnce,useSetFalse,useSetTrue,useToggle}
