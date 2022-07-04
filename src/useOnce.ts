aeae273a220ffc742774cc5c0206f471a6265b00
madness <ss@gmail.com>
2022-07-04 15:26:37 +0300

    useOnce hook implemented

---
+++ src/useOnce.ts
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from 'react'
+
+type OnceCallback = () => void
+const useOnce = (callback: OnceCallback) => {
+	const triggered = useRef(false)
+	useEffect(() => {
+		if (!triggered.current) {
+			callback()
+		}
+	})
+}
+export default useOnce
