a802311dddd3fa421b2eeede02401b847c5a2464
madness <ss@gmail.com>
2022-07-04 17:53:38 +0300

    index.ts support

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import useBool from './useBool'
+import useOnce from './useOnce'
+import useSetTrue from './useSetTrue'
+import useToggle from './useToggle'
+import useSetFalse from './useSetFalse'
+export  {useBool,useOnce,useToggle,useSetTrue,useSetFalse}
