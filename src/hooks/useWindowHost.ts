80cde2dc2d179dadb1460f9ee56c31fb0a90d802
madness <ss@gmail.com>
2022-08-07 14:22:29 +0300

    Fix for Server content do not match hydration error

--- src/hooks/useWindowHost.ts
+++ src/hooks/useWindowHost.ts
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react'
 import useWindow from './useWindow'
 
-const useWindowHost = (): string | null => useWindow()?.location.host??null
+const useWindowHost = (): string | null => {
+	const [host,setHost] = useState<string|null>(null)
+	const window = useWindow()
+	useEffect(()=>{
+		setHost(window?.location.host??null) //Fix for Server content do not match
+	},[window])
+	return host
+}
 export default useWindowHost
